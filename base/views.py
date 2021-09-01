1bf695bb7499b94fae896b3271cdcede08821558
GuildGamer <tobi4steve@gmail.com>
2021-09-01 09:08:34 +0100

    First commit

---
+++ base/views.py
@@ -0,0 +1,8 @@
+#rest-framework imports
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.serializers import Serializer
+#end rest framework imports
