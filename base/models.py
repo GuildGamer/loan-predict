6befb79fc81cbefcb5290e1f9957063b53aea396
GuildGamer <tobi4steve@gmail.com>
2021-09-01 10:26:14 +0100

    API First Commit

--- base/models.py
+++ base/models.py
@@ -1,5 +1,36 @@
 from django.db import models
 from django.conf import settings
 
+GENDER = (
+   ( 'M', 'male'),
+    ('F', 'female')
+)
+
+YES_OR_NO = (
+    ('Y', 'yes'),
+    ('N', 'no')
+)
+
+EDUCATION_STATUS = (
+    ('GR', 'graduate'),
+    ('NT', 'not')
+)
+
+PROPERTY_AREA = (
+    ('UR', 'urban'),
+    ('RU', 'rural')
+)
+
 class Observation(models.Model):
-    pass
+    loan_id= models.CharField(max_length=255)
+    gender = models.CharField(choices=GENDER, max_length=1)	
+    married	= models.CharField(choices=YES_OR_NO, max_length=1)
+    dependents = models.IntegerField()
+    education = models.CharField(choices=EDUCATION_STATUS, max_length=2)
+    self_employed = models.CharField(choices=YES_OR_NO, max_length=1)
+    applicant_income = models.BigIntegerField() 
+    loan_amount = models.BigIntegerField()
+    loan_amount_term = models.BigIntegerField()	
+    credit_history = models.FloatField()
+    property_area = models.CharField(choices=PROPERTY_AREA, max_length=2)
+    loan_status = models.CharField(choices=YES_OR_NO, max_length=1)
--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 #rest-framework imports
+from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -6,3 +7,14 @@ from rest_framework import status
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.serializers import Serializer
 #end rest framework imports
+
+from .serializers import ObservationSerializer
+from .predict import loanpredictfunc
+
+@api_view['POST']
+def predict_view(request):
+    serializer = Serializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    try:
+        response = loanpredictfunc(serializer)
+        Response(response, status=HTTP_200_OK)
